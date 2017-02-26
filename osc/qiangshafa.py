49140ef5e592420b53afcea725c42b0712f8e396
爱吃糖葫芦 <13126506430@163.com>
2017-02-26 12:37:30 +0800

    5.0.0.1
    1.以新旧id作为是否更新的依据；
    2.添加id对象序列化方法；

--- osc/qiangshafa.py
+++ osc/qiangshafa.py
@@ -39,7 +39,7 @@ def qiangshafa(url):
     print(r.read().decode('utf-8'))
 
 flag=False
-def hasnews():
+def hasnews(preid):
     try:
         global flag
         ssl._create_default_https_context = ssl._create_unverified_context
@@ -49,28 +49,34 @@ def hasnews():
         sourp=BeautifulSoup(conn.getresponse().read(),'lxml')
         gilslist=sourp.find_all(class_='time')
         import time
-        #if(time.strftime("%Y/%m/%d", time.localtime()) ==str(gilslist[0].get_text().strip().replace('发布','').strip())):
-        if('2017/02/25' ==str(gilslist[0].get_text().strip().replace('发布','').strip())):
+        id=str(sourp.find_all(class_='blog-title',limit=1)[0]['href']).split('/')[-1]
+        if preid==None:
+            return id
+        if id!=str(preid):
             return str(sourp.find_all(class_='blog-title',limit=1)[0]['href'])
         else:
-            print(time.strftime("%Y-%m-%d", time.localtime()))
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+str(gilslist[0].get_text().strip().replace('发布','').strip()))
             if flag:
                 print('sleep 2 second ...')
                 time.sleep(2)
             else :
                 flag=True
-                date_str=datetime.datetime.now().strftime("%Y-%m-%d 23:59:50")
+                date_str=datetime.datetime.now().strftime("%Y-%m-%d 23:50:50")
                 endtime=datetime.datetime.strptime(date_str,"%Y-%m-%d %H:%M:%S")
                 now=datetime.datetime.now()
                 print('endtime:{end} now:{now}'.format(end=endtime,now=now))
                 interval=(endtime-now)
                 sec = interval.days*24*3600 + interval.seconds
                 print('need sleep {second} second ...'.format(second=abs(sec)))
-                time.sleep(abs(sec))
-            hasnews()
-            print(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
+                time.sleep(sec)
+            return None
+            # hasnews()
+            # print(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime()))
     except ValueError as e:
         sendEmail('发生异常了！'+str(e))
+        fr=open('log.txt')
+        fr.write(str(e))
+        fr.close()
        # sourp.find_all(class_='blog-title',limit=1)['href']
 
 
@@ -92,12 +98,28 @@ def sendEmail(message):
     server.sendmail(from_addr, [to_addr], msg.as_string())
     server.quit()
 
+class log():
+    def __init__(self,id):
+        self.id=id
+id=None
 if __name__ == '__main__':
-    import http.client,datetime
-    id = hasnews().split('/')[-1]
+    global id
+    import http.client,datetime, pickle,os
+    if os.path.exists('dump.txt'):
+        f = open('dump.txt', 'rb')
+        logid = pickle.load(f)
+        preid=logid.id
+        f.close()
+        print('preid is '+str(preid))
+    else:
+        preid=None
+    while id==None:
+        id = hasnews(preid)
+    f = open('dump.txt', 'wb')
+    pickle.dump(log(id), f)
+    f.close()
     url="https://my.oschina.net/action/blog/add_comment?blog={id}".format(id=id)
-    #print(url)
     #https://my.oschina.net/action/blog/add_comment?blog=797134"
     #https://my.oschina.net/xxiaobian/blog/844061
     qiangshafa(url)
-    sendEmail('over'+str(datetime.datetime.now()))
+    sendEmail('over \t'+url+'\t'+str(datetime.datetime.now()))
